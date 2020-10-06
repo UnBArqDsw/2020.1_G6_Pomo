6b1faeaa7dceff76518e9b96dc6953586a8a3260
thallysbraz <thallysbraz3@gmail.com>
2020-10-05 22:39:04 -0300

    corrigindo erro (subi a estrutura do backend no repo errado)

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const port = process.env.PORTA || 3001; //porta que o server esta rodando
-
-//start server NodeJS
-app.listen(port, () => {
-  console.log(`server rodando na porta: ${port}`);
-});
